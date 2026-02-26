12834090ee961846b55539ee025555d46c062114
extermi3939 <franccoo1013@gmail.com>
2026-02-26 15:58:35 -0300

    Fix: Port detection and requirements

--- app.py
+++ app.py
@@ -122,6 +122,7 @@ def delete_file(filename):
     return redirect(url_for('admin_matrix'))
 
 if __name__ == '__main__':
+    # Render asigna un puerto dinámico, esto lo captura correctamente
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
