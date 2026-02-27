edb0a10c7d82b77a3d8e14a412d9191074d63727
extermi3939 <franccoo1013@gmail.com>
2026-02-27 00:50:42 -0300

    Ajuste de puerto para Render

--- app.py
+++ app.py
@@ -207,4 +207,8 @@ def delete_file(filename):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    # Render usa la variable de entorno PORT
+    port = int(os.environ.get("PORT", 5000))
+    # Es VITAL usar host='0.0.0.0' para que sea accesible externamente
+    app.run(host='0.0.0.0', port=port)
+
